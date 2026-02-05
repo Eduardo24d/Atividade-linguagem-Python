f1bbb69c21305ea232f2d53b1dbbb3f2c7fd0fcf
Eduardo <eduardosiqueirat2009@gmail.com>
2026-02-05 11:12:59 -0300

    atividade03

---
+++ atividade03.py
@@ -0,0 +1,4 @@
+funcionario = (str("Jão Carlos"))
+salario = (float(4030.60))
+
+print(f"O Funcionário {funcionario} tem o salário de R$: {salario}.")
---
+++ atividade04.py
@@ -0,0 +1,6 @@
+num01 = int(input("Coloque um número inteiro: "))
+num02 = int(input("Coloque outro número inteiro: "))
+
+soma= num01 + num02
+
+print(f"A soma dos números é: {soma}")
