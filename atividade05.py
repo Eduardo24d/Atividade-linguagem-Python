aa7b1f43040a45b0c169011be83df11fbc00a4ed
Eduardo <eduardosiqueirat2009@gmail.com>
2026-02-05 11:27:57 -0300

    Projeto Inicial

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+Aluno = ("Alvin")
+Notas = [5, 7, 8, 6]
+
+Media = sum(Notas) / len(Notas)
+
+print(f"O Aluno Alvin tem a Média em matemática {Media}")
---
+++ atividade06.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um número inteiro: "))
+numAnt = num-1
+numPos = num+1
+
+print(f"seu numero é: {num}")
+print(f"o número anterior a {num} é: {numAnt}")
+print(f"o número sucessor a {num} é {numPos}")
